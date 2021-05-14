c8883cc1759519e9bc2e75cdbd4d1d06f671ccc4
NatClamp <nat.clamp@gmail.com>
2021-05-14 17:08:02 +0100

    :arrow_up: update deps and remove bodyparser

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const config = { ...process.env };
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
 
